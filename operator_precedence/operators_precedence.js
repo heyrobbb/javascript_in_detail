de744c1c8dedf24f534b7855287d1c942dc09a4b
heyrobbb <rob@robmcelvenny.com>
2025-06-02 15:36:37 -0400

    feat: add operator precedence

---
+++ operator_precedence/operators_precedence.js
@@ -0,0 +1,10 @@
+// store solution of arithmetic calculation in var1 
+var var1 = 1 + 2 * 3;
+console.log(`var1: ${var1}`);
+
+// example two 
+var var2 = 1 + 2 + 3;
+console.log(`var2: ${var2}`);
+
+
+
